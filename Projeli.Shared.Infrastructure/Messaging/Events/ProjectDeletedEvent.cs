22c757b7b56143d8842deba6dca438bf3862a906
DAQEM <daqemyt@gmail.com>
2025-04-09 21:45:26 +0200

    Added project delete event

---
+++ Projeli.Shared.Infrastructure/Messaging/Events/ProjectDeletedEvent.cs
@@ -0,0 +1,9 @@
+﻿using MassTransit;
+
+namespace Projeli.Shared.Infrastructure.Messaging.Events;
+
+[EntityName("ProjectDeleted")]
+public class ProjectDeletedEvent
+{
+    public Ulid ProjectId { get; set; }
+}
